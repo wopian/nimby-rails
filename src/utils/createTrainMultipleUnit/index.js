c6975c54bd2da917944b6217a4c0562fe7f041b5
wopian <wopian@wopian.me>
2021-02-02 04:01:51 +0000

    chore: namespace train mod ids

--- src/utils/createTrainMultipleUnit/index.js
+++ src/utils/createTrainMultipleUnit/index.js
@@ -4,12 +4,12 @@ import { snakeCase } from 'snake-case'
 export const createTrainMultipleUnit = ({ name = 'blank', min_cars = 2, max_cars = 8 } = {}) => ini({
   TrainMultipleUnit: {
     schema: 1,
-    id: `${snakeCase(name)}_mu`,
-    name_loc: `${snakeCase(name)}_name`,
+    id: `wopian_${snakeCase(name)}_mu`,
+    name_loc: `wopian_${snakeCase(name)}_name`,
     name_en: name,
-    head_id: `${snakeCase(name)}_head`,
-    car_id: `${snakeCase(name)}_car`,
-    tail_id: `${snakeCase(name)}_head`,
+    head_id: `wopian_${snakeCase(name)}_head`,
+    car_id: `wopian_${snakeCase(name)}_car`,
+    tail_id: `wopian_${snakeCase(name)}_head`,
     tail_flip: true,
     min_cars,
     max_cars
