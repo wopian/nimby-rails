b6d52bbc8dbdd158ef29886b2559ec02072b6a0b
wopian <wopian@wopian.me>
2021-02-02 22:00:22 +0000

    feat: add seibu railway's fleet

---
+++ data/template/template.js
@@ -0,0 +1,22 @@
+import { TRAIN_TYPE } from '../../src/components/index.js'
+
+export const tobu_railway = {
+  name: 'Template',
+  local: '鋳型',
+  trains: [
+    {
+      wiki: 'https://ja.wikipedia.org/wiki/東武100系電車',
+      train_type: TRAIN_TYPE.LIMITED_EXPRESS,
+      name: 'Train',
+      names: { head: 'Head', car: 'Car' },
+      min_cars: 6,
+      max_cars: 6,
+      max_speed: 120,
+      length: { head: 20, car: 20 },
+      width: 2.9,
+      power: 175,
+      max_pax: { head: 120, car: 150 },
+      empty_mass: { head: 35, car: 37 }
+    }
+  ]
+}
