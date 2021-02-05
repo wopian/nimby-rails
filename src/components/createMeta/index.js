8d7c1545ed3e8365dfba29e7453c53d544a74a59
wopian <wopian@wopian.me>
2021-02-05 01:26:14 +0000

    revert: do not include native name in mod name

--- src/components/createMeta/index.js
+++ src/components/createMeta/index.js
@@ -1,7 +1,7 @@
-export const createMeta = ({ name = 'Mod Name', region, totalTrains = 0, desc, version = '0.0.1' } = {}) => ({
+export const createMeta = ({ name = 'Mod Name', native, region, totalTrains = 0, desc, version = '0.0.1' } = {}) => ({
   ModMeta: {
     schema: 1,
-    name: name,
+    name, // `${name} / ${native}`,
     author: 'wopian',
     desc: desc ? desc : `${totalTrains} EMUs operated in the ${region} region`,
     version
