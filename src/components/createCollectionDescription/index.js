fb5009a8a99fa216135f84722f61b01d9f476f10
wopian <wopian@wopian.me>
2021-02-05 15:57:21 +0000

    test: add tests for createCollectionDescription

--- src/components/createCollectionDescription/index.js
+++ src/components/createCollectionDescription/index.js
@@ -12,24 +12,32 @@ const REGION_COLOURS = {
   'Kyūshū': 'grey'
 }
 
-export const createCollectionDescription = ({ companies } = {}) => {
+export const createCollectionDescription = ({ companies = [], country = 'Japan' } = {}) => {
+  if (companies.length === 0) return ''
   const regions = new Set(companies.map(a => a.region))
   const totalTrains = companies.reduce((a, b) => {
     if (!Number.isInteger(a)) a = 0
-    return a + b.trains.length
+    return a + b?.trains?.length
   })
-  let description = `Collection of ${totalTrains} trains from ${companies.length} companies operating in Japan.\n`
+  let description = `Collection of ${totalTrains} trains from ${companies.length} companies operating in ${country}.\n`
   for (const region of regions) {
-    description += `\n${region} region (${REGION_COLOURS[region]} on map):\n`
+    description += `\n${region} region`
+    if (REGION_COLOURS[region]) description += ` (${REGION_COLOURS[region]} on map)`
+    description += ':\n'
     const filteredCompanies = companies.filter(company => company.region === region)
     for (const company of filteredCompanies) {
-      description += `[url=${company.wiki}]${company.name}[/url]`
-      for (const type in TRAIN_TYPE) {
-        const filteredTrains = filterTrainType({ trains: company.trains, filter: TRAIN_TYPE[type]})
-        if (filteredTrains.length === 0) continue
-        description += ` ${filteredTrains.length} ${type.toLocaleLowerCase()},`
+      if (company.wiki) description += `[url=${company.wiki}]`
+      description += company.name
+      if (company.wiki) description += '[/url]'
+      if (company?.trains?.length > 0) {
+        for (const type in TRAIN_TYPE) {
+          const filteredTrains = filterTrainType({ trains: company.trains, filter: TRAIN_TYPE[type]})
+          if (filteredTrains.length === 0) continue
+          description += ` ${filteredTrains.length} ${type.toLocaleLowerCase()},`
+        }
+        description = `${description.slice(0, -1)}`
       }
-      description = `${description.slice(0, -1)}\n`
+      description += '\n'
     }
   }
   description += '\nCurrently these are using placeholder graphics from the built-in trains. Graphics are planned for a future update.'
---
+++ src/components/createCollectionDescription/index.spec.js
@@ -0,0 +1,39 @@
+import test from 'ava'
+import { TRAIN_TYPE, createCollectionDescription } from '../index.js'
+
+test('blank description gets generated', t => {
+  t.is(createCollectionDescription(), '')
+})
+
+test('with company name, region and country', t => {
+  t.is(createCollectionDescription({
+    companies: [
+      { name: 'Wop Corp', region: 'Cornwall' }
+    ],
+    country: 'the United Kingdom'
+  }), 'Collection of 0 trains from 1 companies operating in the United Kingdom.\n\nCornwall region:\nWop Corp\n\nCurrently these are using placeholder graphics from the built-in trains. Graphics are planned for a future update.')
+})
+
+test('with company wiki', t => {
+  t.is(createCollectionDescription({
+    companies: [
+      { wiki: 'https://wiki.example', name: 'Wop Corp', region: 'Hokkaidō' },
+      { wiki: 'https://wiki2.example', name: 'Ian Corporation', region: 'Tōhoku' }
+    ]
+  }), 'Collection of 0 trains from 2 companies operating in Japan.\n\nHokkaidō region (red on map):\n[url=https://wiki.example]Wop Corp[/url]\n\nTōhoku region (yellow on map):\n[url=https://wiki2.example]Ian Corporation[/url]\n\nCurrently these are using placeholder graphics from the built-in trains. Graphics are planned for a future update.')
+})
+
+test('with company trains', t => {
+  t.is(createCollectionDescription({
+    companies: [
+      {
+        name: 'Wop Corp',
+        region: 'Hokkaidō',
+        trains: [
+          { max_speed: 120 },
+          { max_speed: 300 }
+        ]
+      }
+    ]
+  }), 'Collection of 0 trains from 1 companies operating in Japan.\n\nHokkaidō region (red on map):\nWop Corp 1 highspeed, 1 commuter\n\nCurrently these are using placeholder graphics from the built-in trains. Graphics are planned for a future update.')
+})
