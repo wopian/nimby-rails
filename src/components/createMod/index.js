04e30c66601cf38fd11164c3692c8d5f6e0238e1
wopian <wopian@wopian.me>
2021-03-05 13:48:18 +0000

    chore: remove legacy TrainUnits

--- src/components/createMod/index.js
+++ src/components/createMod/index.js
@@ -1,14 +1,11 @@
 import { ini, createMeta, createTrains, createDescription, sortObject } from '../index.js'
 
-export const createMod = ({ name, native, region, units, trains, legacy }) => {
+export const createMod = ({ name, native, region, units, trains }) => {
   const totalTrains = trains.length
   const totalCompositions = trains.reduce((sum, { composition }) => sum + composition.length, 0)
 
   let output = ini(createMeta({ name, native, region, totalTrains, totalCompositions }))
   output += ini(createTrains(trains, units)).replaceAll('\\#', '#')
-
-  // Schema 1 TrainUnit backwards compatibility
-  if (legacy) output += `\n${legacy}`
 
   const description = createDescription({
     name,
