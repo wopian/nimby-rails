f72b1360109ddb53567bae816112b090ead24a31
wopian <wopian@wopian.me>
2021-03-05 08:40:34 +0000

    refactor: improve price calculation

--- src/components/calculatePrice/index.js
+++ src/components/calculatePrice/index.js
@@ -1,20 +1,35 @@
 import { roundTo } from '../index.js'
 
 export const calculatePrice = ({ max_speed, empty_mass, length, width, max_pax }) => {
-  const M = 0.85095 // Constant tuned for Yamanote E231-500 base-game price of 2_200_000
+  const M = 0.6776 // Constant tuned for Yamanote E231-500 base-game price of 2_200_000
   const dimensions_modifier = Math.cbrt(length) * Math.cbrt(width)
-  const speed_modifier = max_speed <= 70 || max_speed >= 240 ? Math.asinh(max_speed) : Math.cbrt(max_speed)
-  let price = empty_mass * dimensions_modifier * speed_modifier * Math.cbrt(max_pax) * M
+  const speed_modifier = Math.cbrt(max_speed)
+  const pax_modifier = Math.asinh(max_pax <= 50 ? 50 : max_pax)
 
-  if (max_speed <= 70) price *= 1.1
-  else if (max_speed >= 350) price *= 1.75977
-  else if (max_speed >= 240) price *= 0.922
-  else if (max_speed >= 160) price *= 0.69586
+  let price = 1_000_000 * Math.abs(
+    dimensions_modifier + Math.log10(empty_mass) * speed_modifier / Math.log10(pax_modifier)
+  ) * M / 10
 
-  const cost_per_day = Math.round(price / (2_200_000 / 53)) // Yamanote E231-500 price and cost_per_day
+  if (max_speed <= 70) price *= 0.951 // Alstom
+  else if (max_speed <= 90) price *= 1.18 // NYR
+  else if (max_speed <= 100) price *= 0.9102 // London
+  else if (max_speed >= 350) price *= 2.4881 // Siemens
+  else if (max_speed >= 240) price *= 1.0087 // TGV
+  else if (max_speed >= 160) price *= 1.1341 // Renfe - TODO: Maybe change to 220 or 200
+
+  const rounded_price = roundTo(price, 1000)
+  const cost_per_day_modifier = 2_200_000 / 53 // Yamanote E231-500 price and cost_per_day
+
+  let cost_per_day = Math.floor(rounded_price / cost_per_day_modifier)
+
+  if (max_speed > 70 && max_speed <= 90) cost_per_day *= 0.55 // NYR
+  else if (max_speed > 90 && max_speed <= 100) cost_per_day *= 0.96 // London
+  else if (max_speed >= 350) cost_per_day *= 1.43 // Siemens
+  else if (max_speed >= 240) cost_per_day *= 3.34 // TGV
+  else if (max_speed >= 160) cost_per_day *= 2.67 // Renfe - TODO: Maybe change to 220 or 200
 
   return {
-    cost_per_day: cost_per_day >= 20 ? cost_per_day : 20,
-    price: roundTo(price >= 800_000 ? price : 500_000 * Math.cbrt(cost_per_day), 1000)
+    cost_per_day: Math.floor(cost_per_day),
+    price: rounded_price
   }
 }
--- src/components/calculatePrice/index.spec.js
+++ src/components/calculatePrice/index.spec.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import { calculatePrice } from '../index.js'
 
-test('Matches base game prices for Yamanote E231-500', t => {
+test('Yamanote E231-500', t => {
   t.deepEqual(calculatePrice({
     max_speed: 120,
     max_pax: 156,
@@ -14,16 +14,16 @@ test('Matches base game prices for Yamanote E231-500', t => {
   })
 })
 
-test('Renfe121', t => {
+test('AlstomCitadis', t => {
   t.deepEqual(calculatePrice({
-    max_speed: 220,
-    max_pax: 70,
-    empty_mass: 56_250,
-    length: 25.78,
-    width: 2.92
+    max_speed: 70,
+    max_pax: 45,
+    empty_mass: 7000,
+    length: 6.5,
+    width: 2.4
   }), {
-    cost_per_day: 84, // 224
-    price: 3_500_000 // 3_500_000
+    cost_per_day: 40,
+    price: 1_700_000
   })
 })
 
@@ -35,21 +35,8 @@ test('NYR160', t => {
     length: 18.35,
     width: 2.978
   }), {
-    cost_per_day: 69, // 33
-    price: 2_866_000 // 2_500_000
-  })
-})
-
-test('AlstomCitadis', t => {
-  t.deepEqual(calculatePrice({
-    max_speed: 70,
-    max_pax: 45,
-    empty_mass: 7000,
-    length: 6.5,
-    width: 2.4
-  }), {
-    cost_per_day: 20, // 40
-    price: 957_000 // 1_700_000
+    cost_per_day: 33,
+    price: 2_500_000
   })
 })
 
@@ -61,21 +48,8 @@ test('London1992', t => {
     length: 16.25,
     width: 2.62
   }), {
-    cost_per_day: 33, // 42
-    price: 1_359_000 // 1_900_000
-  })
-})
-
-test('TgvDuplex', t => {
-  t.deepEqual(calculatePrice({
-    max_speed: 320,
-    max_pax: 63,
-    empty_mass: 39_000,
-    length: 20,
-    width: 2.904
-  }), {
-    cost_per_day: 73, // 30
-    price: 3_047_000 // 3_400_000
+    cost_per_day: 43, // 42
+    price: 1_900_000
   })
 })
 
@@ -87,8 +61,34 @@ test('SiemensVelaroE', t => {
     length: 25,
     width: 2.95
   }), {
-    cost_per_day: 217, // 308
-    price: 9_000_000 // 9_000_000
+    cost_per_day: 308,
+    price: 9_000_000
+  })
+})
+
+test('TgvDuplex', t => {
+  t.deepEqual(calculatePrice({
+    max_speed: 320,
+    max_pax: 63,
+    empty_mass: 39_000,
+    length: 20,
+    width: 2.904
+  }), {
+    cost_per_day: 270, // 270; 1232 * 2 + 30 * 8
+    price: 3_400_000
+  })
+})
+
+test('Renfe121', t => {
+  t.deepEqual(calculatePrice({
+    max_speed: 220,
+    max_pax: 70,
+    empty_mass: 56_250,
+    length: 25.78,
+    width: 2.92
+  }), {
+    cost_per_day: 224,
+    price: 3_500_000
   })
 })
 
@@ -100,8 +100,8 @@ test('Keikyu DeHa 1500', t => {
     length: 20,
     width: 2.798
   }), {
-    cost_per_day: 52,
-    price: 2_141_000
+    cost_per_day: 54,
+    price: 2_269_000
   })
 })
 
@@ -113,8 +113,8 @@ test('Tokyu 300', t => {
     length: 11.99,
     width: 2.5
   }), {
-    cost_per_day: 26, // 23
-    price: 1_089_000 // 979_896
+    cost_per_day: 38,
+    price: 1_615_000 // 979_896
   })
 })
 
@@ -126,7 +126,7 @@ test('Tokyu DeHa 3220', t => {
     length: 20,
     width: 2.788
   }), {
-    cost_per_day: 69, // 51
-    price: 2_869_000 // 2_490_840
+    cost_per_day: 54, // 51
+    price: 2_251_000 // 2_490_840
   })
 })
